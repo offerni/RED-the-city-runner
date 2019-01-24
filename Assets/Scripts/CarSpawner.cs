497ba94b5f4eec29ede4eb282c09c20a27c0611b
Diego Offerni <drofferni@gmail.com>
2019-01-23 16:50:36 -0800

    Difficulty levels created based on the points earned by the player

--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -10,26 +10,47 @@ public class CarSpawner : MonoBehaviour {
 	[SerializeField] int minSpawnDelay = 1;
 	[SerializeField] int maxSpawnDelay = 5;
 	private int timeBetweenSpawns;
+	private int score;
+
+	private GameSession gameSession;
+	[Header("Difficulties")]
+	[SerializeField] int scoreRegularCar = 750;
+	[SerializeField] int scoreTruck = 1250;
+	[SerializeField] int scoreSpeedCar = 1750;
 
 	// Use this for initialization
 	IEnumerator Start () {
+		gameSession = FindObjectOfType<GameSession>();
+		
 
-		while(spawning) {
-			for (int i = 0; i < carPrefabs.Length; i++) {
-				yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
-				var lastIndex = carPrefabs.Length;
-				var randomCar = Random.Range(0, lastIndex);
-				SpawnCar(carPrefabs[randomCar], randomCar);
+		while (spawning) {
+				for (int i = 0; i < carPrefabs.Length; i++) {
+					yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+					var lastIndex = carPrefabs.Length;
+					var randomCar = Random.Range(0, lastIndex);
+					SpawnCar(carPrefabs[randomCar], randomCar);
+				
 			}
 		}
 	}
 
 	public void SpawnCar(Car car, int carIndex) {
-		if (carIndex == 2) {
-			Instantiate(car, transform.position + (transform.up * 0.5f), transform.rotation);
-		} else {
-			Instantiate(car, transform.position, transform.rotation);
+		if (score > scoreRegularCar) {
+			if (carIndex == 0) {
+				Instantiate(car, transform.position, transform.rotation);
+			}
+		}if (score > scoreTruck) {
+			if (carIndex == 2) {
+				Instantiate(car, transform.position + (transform.up * 0.5f), transform.rotation);
+			}
+		}if (score > scoreSpeedCar) {
+			if (carIndex == 1) {
+				Instantiate(car, transform.position, transform.rotation);
+			}
 		}
-		
+	}
+
+	private void Update() {
+		this.score = gameSession.GetScore();
 	}
 }
