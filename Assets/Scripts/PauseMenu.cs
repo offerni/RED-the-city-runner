9b39c3f40d45834a2176beba60c8ab7d65faf7ad
Diego Offerni <drofferni@gmail.com>
2019-01-31 05:04:19 -0800

    Fixing pause menu overlap

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    private SceneController sceneController;
+    public bool gameIspaused = false;
+    [SerializeField] GameObject pauseMenuUI;
+
+    void Start() {
+        sceneController = FindObjectOfType<SceneController>();
+    }
+
+    // Update is called once per frame
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIspaused = false;
+    }
+
+    public void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIspaused = true;
+    }
+}
